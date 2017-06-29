33b72747acc5e8cdd5c25830dd4805ce4ebcf993
Mate Retsagi <retsagimate@gmail.com>
2017-06-29 22:40:49 +0200

    Add filter by department
    - add event listener to choicebox
    - add select query by department id
    - add update table by department

--- src/com/rmate/employeemanager/service/EmployeeService.java
+++ src/com/rmate/employeemanager/service/EmployeeService.java
@@ -3,12 +3,9 @@ package com.rmate.employeemanager.service;
 import com.rmate.employeemanager.model.Employee;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import oracle.jdbc.driver.OracleDriver;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -48,6 +45,42 @@ public class EmployeeService extends Service {
       con.close();
     } catch (SQLException | ClassNotFoundException ex) {
       System.out.println("exception @getEmployees: " + ex);
+    }
+    
+    return employees;
+  }
+  
+   public static List<Employee> getEmployeesByDepartmentId(int id) {
+    List<Employee> employees = new ArrayList<>();    
+    String query = "SELECT "
+            +         "EMPLOYEE_ID, "
+            +         "FIRST_NAME || ' ' || LAST_NAME AS EMP_NAME, "
+            +         "EMAIL,"
+            +         "HIRE_DATE "
+            +       "FROM "
+            +         "EMPLOYEES "
+            +       "WHERE DEPARTMENT_ID = " + id;
+    
+    try {            
+      Class.forName(DRIVER);
+      Connection con = DriverManager.getConnection(URL, USER, PASSWORD);
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(query);
+      
+      while (rs.next()) {
+        Employee employee = new Employee();
+        
+        employee.setId(rs.getInt("EMPLOYEE_ID"));
+        employee.setName(rs.getString("EMP_NAME"));
+        employee.setEmail(rs.getString("EMAIL"));
+        employee.setHireDate(rs.getDate("HIRE_DATE"));
+        
+        employees.add(employee);
+      }
+      
+      con.close();
+    } catch (SQLException | ClassNotFoundException ex) {
+      System.out.println("exception @getEmployeesByDepId: " + ex);
     }
     
     return employees;
